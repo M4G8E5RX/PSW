28e30e09ae61eaab1e1c5f5f87f776b094bbe846
Gerardo Beltrán <contacto@gerardobeltran.mx>
2020-04-30 09:15:53 -0600

    Update config.php

--- proyectos/proy-02/config.php
+++ proyectos/proy-02/config.php
@@ -1,9 +1,11 @@
 <?php
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT ); // Activa reporte de errores que se presente
 
-    $usuario = "gerardo";
-    $contraseña ="123456789";
-    $servidor = "mysql.miservidor.mx";
-    $basededatos = "gerardo";
- 
+// establece datos de conexion a la base de datos
+$servidor="mysql.miservidor.mx";
+$usuario="userdb";
+$contraseña="12AB34cd*";
+$basededatos="test";
+
 ?>
 
